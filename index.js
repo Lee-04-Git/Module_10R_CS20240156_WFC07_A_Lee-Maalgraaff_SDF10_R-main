4772b573e840e7d5e09332c9f2cfc452b38f665f
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-02 21:12:21 +0200

    add input value to Firebase database on button click

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"
-import config from './config.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const firebaseConfig = {
-    databaseURL: config.DATABASE_URL
+const appSettings = {
+    databaseURL: "https://realtime-database-e0602-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
@@ -12,5 +15,7 @@ const addButtonEl = document.getElementById("add-button")
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
+    push(shoppingListInDB, inputValue)
+
     console.log(inputValue)
 })
