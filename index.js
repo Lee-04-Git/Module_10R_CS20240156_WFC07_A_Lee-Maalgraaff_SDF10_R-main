d21927037e5530587be5d7a955663696382cea6c
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-02 16:03:13 +0200

    selected DOM elements for input, button, and list

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-/*
-Challenge:
-Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
-*/
-
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
-})
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
