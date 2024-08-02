32a373a560f9da5e4c3fd002267d0511322cd10e
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-02 16:12:37 +0200

    reset leads and clear localStorage on delete button double-click

--- index.js
+++ index.js
@@ -32,3 +32,9 @@ function render(leads) {
     }
     ulEl.innerHTML = listItems
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
