6f2da260a25f22a41b2de129b97c5b2b0e0146e5
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-02 21:29:56 +0200

    push input value to Firebase and reset input field

--- index.js
+++ index.js
@@ -9,13 +9,16 @@ const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
+    let inputValue = inputFieldEl.value;
     
-    push(shoppingListInDB, inputValue)
+    push(shoppingListInDB, inputValue);
 
-    console.log(inputValue)
+    inputFieldEl.value = "";
+
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
 })
