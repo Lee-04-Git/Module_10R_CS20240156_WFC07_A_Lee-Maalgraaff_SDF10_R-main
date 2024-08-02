ec7b42d5cbd7e382c22eec8fc7c23fc9d2af6d17
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-02 20:24:04 +0200

    log input field value on button click

--- index.js
+++ index.js
@@ -3,57 +3,14 @@ import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase
 import config from './config.js';
 
 const firebaseConfig = {
-    databaseURL: config.databaseURL
+    databaseURL: config.DATABASE_URL
 }
 
-const app = initializeApp(firebaseConfig)
-const database = getDatabase(app)
-console.log(firebaseConfig.databaseURL)
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
 
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-const deleteBtn = document.getElementById("delete-btn");
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") );
-const tabBtn = document.getElementById("tab-btn");
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
-
-function render(leads) {
-    let listItems = ""
-    for (let i = 0; i < leads.length; i++) {
-        listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `
-    }
-    ulEl.innerHTML = listItems
-}
-
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
-})
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    console.log(inputValue)
 })
