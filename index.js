bd5e4148615a53e78650d29572bf4a7200123686
Lee-04-Git <leemaalgraaff.github@gmail.com>
2024-08-02 17:51:35 +0200

    imported firebase modules and initialize app with config

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"
+import config from './config.js';
+
+const firebaseConfig = {
+    databaseURL: config.databaseURL
+}
+
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+console.log(firebaseConfig.databaseURL)
+
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
